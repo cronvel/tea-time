1d5751d0aafd79d1e96c804bb94ac800185671cd
Cédric Ronvel <cedric.ronvel@gmail.com>
2016-05-22 17:40:51 +0200

    async exception handled

--- sample/test.js
+++ sample/test.js
@@ -3,15 +3,48 @@
 
 describe( "Sync tests" , function() {
 	
-	it( "test #1" , function() {
+	it( "Sync Ok" , function() {
 	} ) ;
 	
-	it( "test #2" , function() {
-		throw new Error( "Error!" ) ;
+	it( "Sync Exception" , function() {
+		throw new Error( "Failed!" ) ;
 	} ) ;
 	
-	it( "test #3" ) ;
+	it( "Pending" ) ;
+} ) ;
+
+
+
+describe( "Async tests" , function() {
 	
+	it( "Async sync Ok" , function( done ) {
+		done() ;
+	} ) ;
+	
+	it( "Async sync Exception" , function( done ) {
+		throw new Error( "Failed!" ) ;
+	} ) ;
+	
+	it( "Async sync fail callback" , function( done ) {
+		done( new Error( 'Failed!' ) ) ;
+	} ) ;
+	
+	it( "Async Ok" , function( done ) {
+		setTimeout( done , 10 ) ;
+	} ) ;
+	
+	it( "Async Exception" , function( done ) {
+		setTimeout( function() {
+			throw new Error( "Failed!" ) ;
+			done() ;
+		} , 10 ) ;
+	} ) ;
+	
+	it( "Async fail callback" , function( done ) {
+		setTimeout( function() {
+			done( new Error( 'Failed!' ) ) ;
+		} , 10 ) ;
+	} ) ;
 } ) ;
 
 
