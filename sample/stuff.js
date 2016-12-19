32f32cee53a7c25aeb4ad2d5def2abcf1e89c1a8
Cédric Ronvel <cedric.ronvel@gmail.com>
2016-12-19 15:14:03 +0100

    Wip: coverage

---
+++ sample/stuff.js
@@ -0,0 +1,17 @@
+
+
+module.exports = function( arg )
+{
+	
+	if ( arg )
+	{
+		// Some comment
+		return 'yeah!' ;
+	}
+	else
+	{
+		// More comment
+		return 'doh!' ;
+	}
+	
+} ;
---
+++ sample/test-cover.js
@@ -0,0 +1,17 @@
+
+
+var stuff = require( "./stuff.js" ) ;
+
+
+
+describe( "Tests" , function() {
+	
+	it( "true" , function() {
+		stuff( true ) ;
+		stuff( true ) ;
+		stuff( true ) ;
+		stuff( true ) ;
+		//stuff( false ) ;
+	} ) ;
+} ) ;
+
